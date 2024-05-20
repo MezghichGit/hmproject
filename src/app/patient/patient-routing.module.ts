625ebbcded31b7595a42fafd9ecc72d8709169e5
haythem <haytheminfo@gmail.com>
2024-05-20 16:21:40 +0100

    Ajout menu Patient

--- src/app/patient/patient-routing.module.ts
+++ src/app/patient/patient-routing.module.ts
@@ -1,8 +1,30 @@
+import { UpdatePatientComponent } from './update-patient/update-patient.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddpatientComponent } from './addpatient/addpatient.component';
+import { ListePatientComponent } from './liste-patient/liste-patient.component';
+import { MainpatientComponent } from './mainpatient/mainpatient.component';
 
 const routes: Routes = [
 
+  {
+    path: '',
+    component: MainpatientComponent,
+    children:[
+      {
+        path: 'listpatient',
+       component: ListePatientComponent,
+      },
+      {
+        path: 'addpatient',
+       component: AddpatientComponent,
+      },
+      {
+        path: 'updatepatient',
+       component: UpdatePatientComponent,
+      }
+    ]
+  }
 
 
 ];
