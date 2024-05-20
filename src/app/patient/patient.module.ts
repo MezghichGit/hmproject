d39556ecdc3114fa8825fad81413f7b1e681499b
Khiari Haifa <khyarihayfa@gmail.com>
2024-05-20 13:54:38 +0100

    creation composant updatePatient

--- src/app/patient/patient.module.ts
+++ src/app/patient/patient.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PatientRoutingModule } from './patient-routing.module';
 import { MainpatientComponent } from './mainpatient/mainpatient.component';
+import { UpdatePatientComponent } from './update-patient/update-patient.component';
 
 
 @NgModule({
   declarations: [
-    MainpatientComponent
+    MainpatientComponent,
+    UpdatePatientComponent
   ],
   imports: [
     CommonModule,
