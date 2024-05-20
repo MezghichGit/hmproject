ec76058463f03a3be63fdff9e7f0d8c3b8676601
khaledmerzougui <50596964+khaledmerzougui@users.noreply.github.com>
2024-05-20 11:49:20 +0100

    ajout route dashboard RDV

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {
+    path: 'dashbaordRdv',
+    loadChildren: () => import('./rdv/rdv.module').then(m => m.RdvModule)
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
