effed9f10b8f19305830f424c01f051266864dd8
khlifi hamda <h.khlifi@gmail.com>
2024-05-20 15:20:14 +0100

    hamda add rdv

--- src/app/rdv/rdv.module.ts
+++ src/app/rdv/rdv.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { RdvRoutingModule } from './rdv-routing.module';
 import { MainrdvComponent } from './mainrdv/mainrdv.component';
+import { AddRDVComponent } from './add-rdv/add-rdv.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-    MainrdvComponent
+    MainrdvComponent,
+    AddRDVComponent,
   ],
   imports: [
     CommonModule,
-    RdvRoutingModule
+    RdvRoutingModule,FormsModule
   ]
 })
 export class RdvModule { }
