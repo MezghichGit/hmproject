192cc11488ca410505dd39a72db4e23ad6de3ff2
Raouf Timoumi <raouftimoumii@gmail.com>
2024-05-20 16:20:08 +0200

    liste des articles

--- src/app/article/list-article/list-article.component.ts
+++ src/app/article/list-article/list-article.component.ts
@@ -5,19 +5,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './list-article.component.html',
   styleUrl: './list-article.component.css'
 })
-export class ProductsComponent implements OnInit {
+export class listarticleComponent implements OnInit {
 
   article!: article[];
   priceMax!   : number;
   p!:number;
-  article: any;
+ //article: any;
   
-  constructor(private service1:ProdService){}
+  constructor( ){}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   
   
  
-  ngOnInit(): void {
-    this.service1.getproduct().subscribe(data=>this.article=data);
-  }
+  // ngOnInit(): void {
+  //   this.service1.getproduct().subscribe(data=>this.article=data);
+  // }
 }
     
