297dd485927d77b855dc767bce3e380650793cc9
khaledmerzougui <50596964+khaledmerzougui@users.noreply.github.com>
2024-05-20 15:42:22 +0100

    add navbar rdv

--- src/app/rdv/rdv-routing.module.ts
+++ src/app/rdv/rdv-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddRDVComponent } from './add-rdv/add-rdv.component';
+import { ListRdvComponent } from './list-rdv/list-rdv.component';
+import { MainrdvComponent } from './mainrdv/mainrdv.component';
+import { UpdateRDVComponent } from './update-rdv/update-rdv.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: MainrdvComponent,
+    children:[
+      {
+        path: 'list',
+       component: ListRdvComponent,
+      },
+      {
+        path: 'add',
+       component: AddRDVComponent,
+      },
+      {
+        path: 'update',
+       component: UpdateRDVComponent,
+      }
+    ]
+  }
  
 ];
 
